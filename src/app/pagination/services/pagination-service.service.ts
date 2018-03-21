f181c215e8b158211ee98367ad407721a95448d9
Osmar <osmargv99@gmail.com>
2018-03-21 00:58:02 -0300

    alteracoes

---
+++ src/app/pagination/services/pagination-service.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Ipage } from '../interface/ipage';
+
+@Injectable()
+export class PaginationServiceService {
+
+  constructor() { }
+
+
+  tratarPaginacao(pagination:Ipage) {
+    let qtdRegisterPage = 10;
+    pagination.pages = [];
+    pagination.totalPages = Math.ceil(pagination.totalRegisters / qtdRegisterPage);
+    let startPage: number, endPage: number;
+    //se for menor ou igual a 10 show all 
+    if (pagination.totalPages <= 10) {
+      startPage = 1;
+      endPage = pagination.totalPages;
+    }
+    else {
+      // more than 10 total pages so calculate start and end pages
+      if (pagination.currentPage <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (pagination.currentPage + 4 >= pagination.totalPages) {
+        startPage = pagination.totalPages - 9;
+        endPage = pagination.totalPages;
+      } else {
+        startPage = pagination.currentPage - 5;
+        endPage = pagination.currentPage + 4;
+      }
+    }
+
+    // calculate start and end item indexes
+    let startIndex = (pagination.currentPage - 1) * qtdRegisterPage;
+    let endIndex = Math.min(startIndex + qtdRegisterPage - 1, pagination.totalRegisters - 1);
+
+    for (let i = startPage; i < (endPage + 1); i++) {
+      pagination.pages.push({
+        description: i.toString(),
+        numberPage: i,
+        currentPage: i == pagination.currentPage ? true : false
+      });
+    }
+  }
+
+}
